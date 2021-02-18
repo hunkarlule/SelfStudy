8f7990ae1ec89d3348e078912086886068f766d7
hlule <l.hunkar@gmail.com>
2021-02-18 15:41:07 -0500

    recursive function

--- prog.cpp
+++ prog.cpp
@@ -12,20 +12,37 @@ using namespace std;
 //	return a + b;
 //}
 
-template<typename Atype, typename Btype>
-auto add(Atype a, Btype b) -> decltype(a + b)
+//template<typename Atype, typename Btype>
+//auto add(Atype a, Btype b) -> decltype(a + b)
+//{
+//	return a + b;
+//}
+
+unsigned int fact(unsigned int x)
 {
-	return a + b;
+	if (x == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return x * fact(x - 1);
+	}
 }
 
 int main()
 {
-	int x = 10;
-	int y = 20;
-	int z;
+	//int x = 10;
+	//int y = 20;
+	//int z;
 
-	z = add(x, y);
-	cout << z << endl;
+	//z = add(x, y);
+	//cout << z << endl;
+
+	unsigned int v;
+	v = fact(4);
+
+	cout << v << endl;
 
 	return 0;
 
