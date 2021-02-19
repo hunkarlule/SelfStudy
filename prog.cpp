54e1b40afa45380c292e9708b9d04357797516fc
hlule <l.hunkar@gmail.com>
2021-02-19 04:37:00 -0500

    function pointer array example

--- prog.cpp
+++ prog.cpp
@@ -2,87 +2,33 @@
 #include <iostream>
 using namespace std;
 
-int add(int a, int b)
+void add(int a, int b)
 {
-	return a + b;
+	cout << a << "+" << b << "=" << (a + b) << endl;
 }
-//void foo()
-//{
-//	add(1, 20);
-//}
 
-//auto add(int a, int b) -> int
-//{
-//	return a + b;
-//}
-
-//template<typename Atype, typename Btype>
-//auto add(Atype a, Btype b) -> decltype(a + b)
-//{
-//	return a + b;
-//}
-
-//unsigned int fact(unsigned int x)
-//{
-//	if (x == 1)
-//	{
-//		return 1;
-//	}
-//	else
-//	{
-//		return x * fact(x - 1);
-//	}
-//}
-
-//void hanoi(int n, char ta, char tb, char tc)
-//{
-//	static int row = 1;
-//	if (n == 1)
-//	{
-//		cout << row++ <<"- move from " << ta << " to " << tb << endl;
-//	}
-//	else
-//	{
-//		hanoi(n - 1, ta, tc, tb);
-//		cout << row++ << "- move from " << ta << " to " << tb << endl;
-//		hanoi(n - 1, tc, tb, ta);
-//	}
-//}
-
-int dvd(int a, int b)
+void sub(int a, int b)
 {
-	return a / b;
+	cout << a << "-" << b << "=" << (a - b) << endl;
 }
+
+void mul(int a, int b)
+{
+	cout << a << "*" << b << "=" << (a * b) << endl;
+}
+
+void divid(int a, int b)
+{
+	cout << a << "/" << b << "=" << (a / b) << endl;
+}
+
 int main()
 {
-	//int x = 10;
-	//int y = 20;
-	//int z;
-
-	//z = add(x, y);
-	//cout << z << endl;
-	//cout << unsigned int(&z) << endl;
-	//cout << unsigned int(add) << endl;
-
-	//unsigned int v;
-	//v = fact(4);
-
-	//cout << v << endl;
-
-	//hanoi(4, 'A', 'B', 'C');
-
-	//foo();
-
-	int v;
-	int (*funcPtr)(int, int);
-	funcPtr = add;
-	v = funcPtr(3, 4);
-	cout << v << endl;
-
-	funcPtr = dvd;
-	v = funcPtr(4, 2);
-	cout << v << endl;
+	void (*func[4])(int a, int b) = {add, sub, mul, divid};
+	for (int i = 0; i < 4; i++)
+	{
+		func[i](10, 2);
+	}
 
 	return 0;
-
 }
